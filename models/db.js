3fe3eb6e7780ff4ed5f9a5b4a98bc92817406e52
Ainz Ooal Gown <matthias@earthman-music.de>
2022-03-07 14:22:29 +0100

    Merge pull request #1
    * Database Design was updated, so the models and their methods had to b…

--- models/db.js
+++ models/db.js
@@ -4,11 +4,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
 });
 
 connection.connect();
+
+export function asyncQuery(sql, args) {
+  return new Promise((resolve, reject) => {
+    connection.query(sql, args, (err, rows) => {
+      if (err) return reject(err);
+      resolve(rows);
+    })
+  })
+}
 
 export default connection;
--- routes/kassenanweisungen.js
+++ routes/kassenanweisungen.js
@@ -5,40 +5,40 @@ import * as pdfservice from "../services/KassenanweisungPDF.js";
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('kassenanweisung')
+  res.render('kassenanweisung')
 });
 
 router.post('/', (req, res) => {
-    model.insertKaWe(req, res);
+  model.insertKaWe(req, res);
 })
 
 router.get('/kaweanzeigen', (req, res) => {
-    model.viewAllKaWe(req, res);
+  model.viewAllKaWe(req, res);
 })
 
 router.get('/delete/:id', (req, res) => {
-    model.deleteKaWe(req, res);
+  model.deleteKaWe(req, res);
 })
 
-router.get('/edit/:id', (req,res) => {
-    model.viewEditKaWe(req,res);
+router.get('/edit/:id', (req, res) => {
+  model.viewEditKaWe(req, res);
 })
 
-router.post('/edit/:id', (req,res) => {
-    model.updateKaWe(req, res);
+router.post('/edit/:id', (req, res) => {
+  model.updateKaWe(req, res);
 })
 
 router.get('/view/:id', (req, res) => {
-    model.viewKaWe(req, res);
+  model.viewKaWe(req, res);
 })
 
 router.get('/kawedownload', (req, res) => {
-    pdfservice.kassenanweisungdownload(req, res);
+  pdfservice.kassenanweisungdownload(req, res);
 })
 
 router.get('*', (req, res) => {
-    res.render('404');
+  res.render('404');
 })
 
 
-export {router};
+export { router };
